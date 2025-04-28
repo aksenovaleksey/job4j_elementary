e92a50dc93a33a55beac9427186358350ec79fcf
aksenovaleksey <Aksenov_AV@bk.ru>
2025-04-28 20:26:15 +0300

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,18 +3,21 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        // Formula for man's weight: (height in cm - 100) * 1.15
-        return (height - 100) * 1.15;
+              return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        // Formula for woman's weight: (height in cm - 110) * 1.15
-        return (height - 110) * 1.15;
+                return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 cm is " + man + " kg");
+    }
+    public static void main(String[] args) {
+        short height = 170;
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman 170 cm is " + woman + " kg");
     }
 }
--- src/main/java/ru/job4j/calculator/calculator.java
+++ src/main/java/ru/job4j/calculator/calculator.java
@@ -8,15 +8,18 @@ public class MathCalculator {
         return sum(first, second)
                 + multiply(first, second);
     }
+
     public static double  sumDifferenceAndDivision(double first, double second) {
         return difference(first, second)
                 + division(first, second);
     }
+
     public static double sumAll(double first, double second) {
         return sum(first, second)
-                + multiply (first, second);
-                + difference(first, second);
-                + division(first, second);
+                + multiply(first, second);
+        +difference(first, second);
+        +division(first, second);
+    }
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
