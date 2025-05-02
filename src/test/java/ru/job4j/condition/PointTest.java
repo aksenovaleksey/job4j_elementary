ecbf67f1c2928f14ad54249100a068623ea57bf6
aksenovaleksey <Aksenov_AV@bk.ru>
2025-05-02 05:26:24 +0300

    Тестирование Класса Расстояния между Точками [#505280]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void distance() {
+    }
+
+    @Test
+    void main() {
+    }
+}
