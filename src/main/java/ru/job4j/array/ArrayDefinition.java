231112a28fb83988c00678f9cf0d009d802a1fad
aksenovaleksey <Aksenov_AV@bk.ru>
2025-05-18 19:36:35 +0300

    Объявление массива [#505220]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,12 +1,12 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-            System.out.println("ages" + ages);
-            System.out.println("surnames" + surnames);
-            System.out.println("prices" + prices);
+            System.out.println("ages" + ages.length);
+            System.out.println("surnames" + surnames.length);
+            System.out.println("prices" + prices.length);
     }
 }
