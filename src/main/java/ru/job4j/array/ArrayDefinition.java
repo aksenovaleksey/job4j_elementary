0829743b9766d88c7b01af51ff4f7885e74a2085
aksenovaleksey <Aksenov_AV@bk.ru>
2025-05-19 20:25:05 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,17 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         String[] names = new String[4];
+        float[] prices = new float[40];
         names [0] = "Petr Arsentev";
         names [1] = "Aleksei Aksenov";
         names [2] = "Poul Smith";
         names [3] = "John Steel";
-        float[] prices = new float[40];
             System.out.println("Размер массива ages равен:" + ages.length);
             System.out.println("Размер массива surnames равен:" + surnames.length);
-            System.out.println(names[0]);
-            System.out.println(names[1]);
-            System.out.println(names[2]);
-            System.out.println(names[3]);
             System.out.println("Размер массива prices равен:" + prices.length);
+            System.out.println("Имена:");
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
