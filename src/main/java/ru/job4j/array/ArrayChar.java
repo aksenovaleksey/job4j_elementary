73ad62570ea74f0a24acb1b44be481ba4fab9e06
aksenovaleksey <Aksenov_AV@bk.ru>
2025-05-21 21:06:48 +0300

    Слово начинается с ... [#505233]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        if (prefix.length > word.length) {
+            result = false;
+        } else {
+            for (int i = 0; i < prefix.length; i++) {
+                if (word[i] != prefix[i]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
