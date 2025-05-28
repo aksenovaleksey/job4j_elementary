8e058c5fc826af2123b6186850903f8dc4ca70df
aksenovaleksey <Aksenov_AV@bk.ru>
2025-05-28 11:46:49 +0300

    6.7.4. Выигрышные комбинации в сокобан [#505248 #544772]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -32,25 +32,13 @@ public class MatrixCheck {
     }
 
     public static boolean isWin(char[][] board) {
-            for (int row = 0; row < board.length; row++) {
-            if (board[row][row] == 'X') {
-                boolean rowWin = true;
-                for (int cell = 0; cell < board[row].length; cell++) {
-                    if (board[row][cell] != 'X') {
-                        rowWin = false;
-                        break;
-                    }
-                }
-                boolean cellWin = true;
-                for (int cell = 0; cell < board.length; cell++) {
-                    if (board[cell][row] != 'X') {
-                        cellWin = false;
-                        break;
-                    }
-                }
-                return rowWin || cellWin;
+        boolean result = false;
+            for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
             }
         }
-        return false;
+        return result;
     }
    }
