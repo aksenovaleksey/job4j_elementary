13e91f42a543e72f71151dc1829e9514d3e44944
aksenovaleksey <Aksenov_AV@bk.ru>
2025-07-02 07:30:08 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -24,10 +24,7 @@ public class Point {
     }
 
     public double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2)
-                + pow(this.y - that.y, 2)
-                + pow(this.z - that.z, 2)
-                + pow(this.z - that.z, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static double distance3d(Point first, Point second) {
